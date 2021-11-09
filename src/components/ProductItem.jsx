2a9c6212880e92ba69dccd8197a383b6c9ca8f81
Jhonatan Mideros <10jonmid@gmail.com>
2021-11-09 17:52:01 -0500

    [add] state a component header and ProducItem

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@styles/ProductItem.scss';
 
 import addCart from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
+  const [cart, setCart] = useState([]);
+
+  const handleClick = () => {
+    setCart([]);
+  };
+
   return (
     <div className='ProductItem'>
       <img src='https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' alt='' />
@@ -12,8 +18,8 @@ const ProductItem = () => {
           <p>$120,00</p>
           <p>Bike</p>
         </div>
-        <figure>
-          <img src={addCart} alt='' />
+        <figure onClick={handleClick}>
+          <img src={addCart} alt='add-to-cart' />
         </figure>
       </div>
     </div>
