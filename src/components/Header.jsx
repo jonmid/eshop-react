fbcf18a8af0d6eb0e51a31d68c9721937eab0748
Jhonatan Mideros <10jonmid@gmail.com>
2021-11-10 16:34:03 -0500

    [add] Se adiciono configuracion para manejar el context de la aplicacion y se manipulo desde varios componentes.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
-import '@styles/Header.scss';
+import React, { useState, useContext } from 'react';
 
+import '@styles/Header.scss';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
-
 import Menu from '@components/Menu';
+import MyOrder from '@containers/MyOrder';
+import AppContext from '../context/AppContext';
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
+  const [toggleOrders, setToggleOrders] = useState(false);
+  const { state } = useContext(AppContext);
 
   const handleToggle = () => {
     setToggle(!toggle);
@@ -45,13 +48,14 @@ const Header = () => {
           <li className='navbar-email' onClick={handleToggle}>
             email@example.com
           </li>
-          <li className='navbar-shopping-cart'>
+          <li className='navbar-shopping-cart' onClick={() => setToggleOrders(!toggleOrders)}>
             <img src={shoppingCart} alt='shopping cart' />
-            <div>2</div>
+            {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
           </li>
         </ul>
       </div>
       {toggle && <Menu />}
+      {toggleOrders && <MyOrder />}
     </nav>
   );
 };
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import '../styles/global.css';
+import AppContext from '../context/AppContext';
+import useInitialState from '../hooks/useInitialState';
+
 import Layout from '../containers/Layout';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
@@ -15,23 +18,27 @@ import Orders from '../pages/Orders';
 import NotFound from '../pages/NotFound';
 
 const App = () => {
+  const initialState = useInitialState();
+
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/login' element={<Login />} />
-          <Route exact path='/password-recovery' element={<PasswordRecovery />} />
-          <Route exact path='/send-email' element={<SendEmail />} />
-          <Route exact path='/new-password' element={<NewPassword />} />
-          <Route exact path='/account' element={<MyAccount />} />
-          <Route exact path='/signup' element={<CreateAccount />} />
-          <Route exact path='/checkout' element={<Checkout />} />
-          <Route exact path='/orders' element={<Orders />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/login' element={<Login />} />
+            <Route exact path='/password-recovery' element={<PasswordRecovery />} />
+            <Route exact path='/send-email' element={<SendEmail />} />
+            <Route exact path='/new-password' element={<NewPassword />} />
+            <Route exact path='/account' element={<MyAccount />} />
+            <Route exact path='/signup' element={<CreateAccount />} />
+            <Route exact path='/checkout' element={<Checkout />} />
+            <Route exact path='/orders' element={<Orders />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 };
 
